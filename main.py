db27ad7ed29ad6edc65b1c938020c763bd8e126e
Meng Li <meng.li.cyber@gmail.com>
2026-05-07 09:23:39 +0300

    using ollama host on windows

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-# 
+import os
+
 from typing import List
 from pydantic import BaseModel, Field
 from langchain.agents import create_agent
@@ -11,6 +12,7 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
+
 
 class Source(BaseModel):
     """ Schema for a source used by agent"""
@@ -21,17 +23,32 @@ class AgentResponse(BaseModel):
     """ Schema for agent response with answer and souces"""
 
     answer:str = Field(description="The agent's answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources used to generate answers")
 
 
 # llm = ChatOpenAI(model="gpt-5-mini")
-llm = ChatOllama(model="gpt-oss:20b", temperature=0.2)
+llm = ChatOllama(model="gpt-oss:20b", base_url=os.getenv("OLLAMA_HOST_URL"), temperature=0.2)
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
     # print("Hello, World!")
-    response = agent.invoke({"messages": HumanMessage(content="list 5 jobs posting on linkedin for a ai engineer agentic ai applications in Colorado Springs")})
-    print(response)
+    # response = agent.invoke({"messages": HumanMessage(content="list 5 jobs posting on linkedin for a ai engineer agentic ai applications in Colorado Springs")})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content= (
+                "List 5 LinkedIn job postings for AI Engineer roles focused on "
+                "agentic AI applications in the Colorado Springs area. "
+                "For each posting, include the job title, company, location, brief summary, "
+                "required skills, and link if available."
+                )
+            )
+        }
+    )
+    # result = agent.invoke({"messages": HumanMessage(content="give me the lastest 3 news on CNN?")})
+
+    print(result)
 if __name__ == "__main__":
     main()
